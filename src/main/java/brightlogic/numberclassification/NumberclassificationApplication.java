d958942dd6d2314850eed99e173bc7e97ed033f5
jonah odoh <jonahodoh@gmail.com>
2025-02-03 20:28:04 +0100

    first commit

---
+++ src/main/java/brightlogic/numberclassification/NumberclassificationApplication.java
@@ -0,0 +1,13 @@
+package brightlogic.numberclassification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberclassificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberclassificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brightlogic/numberclassification/NumberclassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package brightlogic.numberclassification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberclassificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
