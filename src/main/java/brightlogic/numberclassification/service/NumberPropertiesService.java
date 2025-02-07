0a62646aabb89b844031c87591321839170ffdc1
jonah odoh <jonahodoh@gmail.com>
2025-02-07 21:06:48 +0100

    second hng task completed

--- src/main/java/brightlogic/numberclassification/service/NumberPropertiesService.java
+++ src/main/java/brightlogic/numberclassification/service/NumberPropertiesService.java
@@ -2,9 +2,8 @@ package brightlogic.numberclassification.service;
 
 
 import brightlogic.numberclassification.dto.NumberPropertiesRequest;
-import brightlogic.numberclassification.dto.NumberPropertiesResponse;
 
 public interface NumberPropertiesService {
-//    NumberPropertiesResponse classifyNumber(int number);
+
     NumberPropertiesRequest getClassifyNumber(int number);
 }
